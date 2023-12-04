cd89397b0496116dd08bf60d97a0cbbb22177181
Andreau Aranton <aoaranton@up.edu.ph>
2023-12-04 21:34:05 +0800

    finished exer 9

--- Final/Exercise 8/exer9.py
+++ Final/Exercise 8/exer9.py
@@ -11,8 +11,8 @@ S = 60
 # A low number also results in input lag, as input information is processed once per frame.
 FPS = 120
 
-w, h = 360, 240
-fbRange = [6200, 6800]
+w, h = 960, 640
+fbRange = [25000, 35000]
 pid = [0.4, 0.4, 0]
 pError = 0
 isFollowingFace = False
@@ -23,7 +23,7 @@ class FrontEnd(object):
         pygame.init()
 
         # Create pygame window
-        pygame.display.set_caption("Exercise 8")
+        pygame.display.set_caption("Exercise 9")
         self.screen = pygame.display.set_mode([960, 720])
 
         # Init Tello object that interacts with the Tello drone
@@ -37,6 +37,7 @@ class FrontEnd(object):
         self.speed = 10
 
         self.send_rc_control = False
+        self.face_tracking_active = False
 
         # create update timer
         pygame.time.set_timer(pygame.USEREVENT + 1, 1000 // FPS)
@@ -96,19 +97,33 @@ class FrontEnd(object):
                         1,
                         (0, 0, 255),
                         2)
+            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            
+            # frame, info = self.findFace(frame)
+            
+
+            if self.face_tracking_active:
+                global pError
+                frame, info = self.findFace(frame_read.frame)
+                pError = self.trackFace(info, w, pid, pError)
+
+
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             frame = np.rot90(frame)
             frame = np.flipud(frame)
-
+            
             frame = pygame.surfarray.make_surface(frame)
             self.screen.blit(frame, (0, 0))
+
             for buttons in [up_button, down_button,
-                            rot_left_button, rot_right_button,
-                            forward_button, backward_button,
-                            left_button, right_button,
-                            takeoff_button, land_button]:
-                buttons.update(self.screen)
+                        rot_left_button, rot_right_button,
+                        forward_button, backward_button,
+                        left_button, right_button,
+                        takeoff_button, land_button]:
+                        buttons.update(self.screen)
             pygame.display.update()
+
+
 
             time.sleep(1 / FPS)
 
@@ -135,7 +150,8 @@ class FrontEnd(object):
         elif key == pygame.K_s:  # set yaw clockwise velocity
             self.yaw_velocity = S
         elif key == pygame.K_f:  # start face tracking
-            self.startFaceTracking()
+           self.toggleFaceTracking()
+
 
     def keyup(self, key):
         # Update velocities based on key released
@@ -162,16 +178,24 @@ class FrontEnd(object):
                                        self.for_back_velocity,
                                        self.up_down_velocity,
                                        self.yaw_velocity)
-    def startFaceTracking(self):
+    def toggleFaceTracking(self):
         global pError
-        frames = self.tello.get_frame_read()
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_f:
-                    return  # Break out of the loop if 'F' key is pressed
-            frame, info = self.findFace(frames.frame)
-            pError = self.trackFace(self, info, w, pid, pError)
-        return
+        self.face_tracking_active = not self.face_tracking_active
+        if self.face_tracking_active:
+            pError = 0
+    
+
+    # def startFaceTracking(self):
+    #     global pError
+    #     frames = self.tello.get_frame_read()
+    #     while True:
+    #         for event in pygame.event.get():
+    #             if event.type == pygame.KEYDOWN and event.key == pygame.K_f:
+    #                 return  # Break out of the loop if 'F' key is pressed
+    #         frame, info = self.findFace(frames.frame)
+    #         pError = self.trackFace(info, w, pid, pError)
+           
+    #     return
     
     def findFace(self,img):
         faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -182,12 +206,14 @@ class FrontEnd(object):
         myFaceListArea = []
 
         for(x,y,w,h) in faces:
+            print(x,y,w,h)
             cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
             cx = x + w//2
             cy = y + h//2
             area = w*h
             myFaceListArea.append(area)
             myFaceListC.append([cx,cy])
+    
     
         if len(myFaceListArea) !=0:
             i = myFaceListArea.index(max(myFaceListArea))
@@ -199,6 +225,7 @@ class FrontEnd(object):
 
         area = info[1]
         x,y = info[0]
+        fb = 0
 
         error = x - w//2
         speed = pid[0]*error + pid[1]*(error-pError)
@@ -208,15 +235,17 @@ class FrontEnd(object):
         if area > fbRange[0] and area < fbRange[1]:
             fb = 0
         elif area > fbRange[1]:
-            fb = -20
+            fb = -60
         elif area < fbRange[0] and area != 0:
-            fb = 20
+            fb = 60
 
         if x == 0:
             speed = 0
             error = 0
 
-        self.send_rc_control(0,fb, 0, speed)
+        # print("FB, speed", fb, speed)
+
+        self.tello.send_rc_control(0,fb, 0, speed)
 
         return error
 
